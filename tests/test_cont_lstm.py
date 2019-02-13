ea9269eaffb4af0c5b58e8924be33361b76e7f2d
Michał Jóźwiak <michal.jozwiak95@gmail.com>
2019-02-13 13:48:03 +0100

    More tests

--- tests/test_cont_lstm.py
+++ tests/test_cont_lstm.py
@@ -14,6 +14,16 @@ def cstm_cell(request):
 def cstm_trainer(cstm_cell):
     return ContLSTMTrainer(cstm_cell)
 
+@pytest.fixture(
+    scope='module',
+    params=[
+        (np.array([2, 3, 1, 0]), np.array([[0, 2], [2, 5], [5, 6], [6, 6]])),
+        (np.array([0, 0, 4, 5, 1, 6]), np.array([[0, 0], [0, 0], [0, 4], [4, 9], [9, 10], [10, 16]]))
+    ]
+)
+def counts_data(request):
+    yield request.param
+
 
 @pytest.mark.usefixtures('random_seed')
 def test_get_inter_t_mask(tf_session, cstm_trainer):
@@ -33,3 +43,11 @@ def test_get_inter_t_mask(tf_session, cstm_trainer):
         res_np = np.transpose(res_np, (2, 0, 1))  # batch x N x S -> S x batch x N
 
         assert np.array_equal(res_tf, res_np)
+
+def test_convert_counts_to_slices(tf_session, cstm_trainer, counts_data):
+    TEST_DATA, TEST_RES = counts_data
+
+    res = cstm_trainer._convert_counts_to_slices(TEST_DATA)
+    res = tf_session.run(res)
+
+    assert np.array_equal(res, TEST_RES)

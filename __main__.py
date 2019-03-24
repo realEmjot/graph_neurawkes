b696230f8153f3b9661ec7948b479296be302bdc
Michał Jóźwiak <michal.jozwiak95@gmail.com>
2019-03-24 18:22:14 +0100

    UI beginning

---
+++ __main__.py
@@ -0,0 +1,104 @@
+import functools
+import os
+import pickle
+
+import tensorflow as tf
+
+import data.edgelist_data.utils as edgelist_utils
+from src import Neurawkes, GraphNeurawkes
+
+
+_mode = None  # global on purpose!
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+
+def _check_mode_decorator(expected_mode):
+    global _mode
+
+    if _mode is None:
+        _mode = expected_mode
+    elif _mode != expected_mode:
+        raise RuntimeError('You cannot both train the model and generate'
+                            'from it using the same python session :(')
+
+def train_and_save(
+        data_path,
+        num_epochs, batch_size,
+        num_units, num_types,
+        N_ratio=1.0,
+        vstate_len=None,
+        dataset_size=None, val_ratio=None,
+        results_savepath=None, model_savepath=None,
+        data_type='edgelist',
+        data_mode='full',
+        batching_mode=None,
+        batching_kwargs=None):
+
+    _check_mode_decorator('static')
+
+    if data_type == 'edgelist':
+        if batching_mode is None:
+            cut_func=None
+        else:
+            if batching_mode == 'gap_cut':
+                cut_func = edgelist_utils.cut_on_big_gaps
+            else:
+                raise NotImplementedError
+
+            if batching_kwargs is None:
+                batching_kwargs = {}
+            else:
+                pass # TODO make this somehow inputable from command line
+        
+        if data_mode == 'full':
+            ds_func = edgelist_utils.to_event_dataset_full
+            model_class = GraphNeurawkes
+        elif data_mode == 'sender_only':
+            ds_func = edgelist_utils.to_event_dataset_sender_only
+            model_class = Neurawkes
+        elif data_mode == 'naive':
+            ds_func = edgelist_utils.to_event_dataset_naive
+            model_class = Neurawkes
+        else:
+            raise NotImplementedError
+    else:
+        raise NotImplementedError
+
+    if model_class is Neurawkes:
+        model = Neurawkes(num_units, num_types)
+    if model_class is GraphNeurawkes:
+        if vstate_len is None:
+            raise ValueError
+        model = GraphNeurawkes(num_units, num_types, vstate_len)
+
+    dataset, dataset_size = ds_func(data_path, cut_func, **batching_kwargs)
+
+    with tf.Session() as sess:
+        result = model.train(
+            sess, dataset, N_ratio, num_epochs, batch_size, dataset_size,
+            val_ratio, model_savepath
+        )
+
+    if model_savepath:
+        with open(results_savepath, 'wb') as f:
+            pickle.dump(result, f)
+
+
+def generate():
+    _check_mode_decorator('eager')
+    pass
+    
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        prog='Dynamic network generator',
+        description='Generates dynamic networks, using Graph Neurawkes model. '
+                     'Vanilla Neurawkes model is included as well'
+    )
+
+    args = parser.parse_args()
+
+    # TODO
--- data/edgelist_data/utils.py
+++ data/edgelist_data/utils.py
@@ -6,6 +6,11 @@ import tensorflow as tf
 def _get_pair_id(i, j, num_ids):
     return num_ids * i + j
 
+def _get_event_ids(df, num_ids):
+    return np.array([_get_pair_id(i, j, num_ids)
+                    for i, j in zip(df.sender, df.recipient)],
+                    dtype=np.int32)
+
 def _get_df(filepath):
     df = pd.read_csv(filepath, names=['sender', 'recipient', 'time'],
                        dtype={'time': np.float32})
@@ -19,9 +24,9 @@ def _get_df(filepath):
 
     return df
 
-def _cut_df(df, difference_cut, min_len=2):
+def cut_on_big_gaps(df, min_gap_size, min_len=2):
     t_diffs = np.array([t2 - t1 for t1, t2 in zip(df.time[:-1], df.time[1:])])
-    boundaries = np.nonzero(t_diffs >= difference_cut)[0] + 1
+    boundaries = np.nonzero(t_diffs >= min_gap_size)[0] + 1
 
     dfs = []
     for start, end in zip(np.insert(boundaries, 0, 0), np.append(boundaries, len(df))):
@@ -32,17 +37,12 @@ def _cut_df(df, difference_cut, min_len=2):
             dfs.append(df_slice)
     return dfs
 
-def _get_event_ids(df, num_ids):
-    return np.array([_get_pair_id(i, j, num_ids)
-                    for i, j in zip(df.sender, df.recipient)],
-                    dtype=np.int32)
-
-def to_event_dataset_naive(filepath, difference_cut=None):
+def to_event_dataset_naive(filepath, cut_func=None, **cut_kwargs):
     df = _get_df(filepath)
     num_ids = max(df.sender.max(), df.recipient.max()) + 1
 
-    if difference_cut:
-        dfs = _cut_df(df, difference_cut)
+    if cut_func:
+        dfs = cut_func(df, **cut_kwargs)
         data = [(_get_event_ids(df, num_ids), df.time, df.time.max()) for df in dfs]
         ds = tf.data.Dataset.from_generator(lambda: data, (tf.int32, tf.float32, tf.float32))
         return ds, len(dfs)
@@ -50,10 +50,10 @@ def to_event_dataset_naive(filepath, difference_cut=None):
     ds = tf.data.Dataset.from_tensors((_get_event_ids(df, num_ids), df.time, df.time.max()))
     return ds, 1
 
-def to_event_dataset_sender_only(filepath, difference_cut=None):
+def to_event_dataset_sender_only(filepath, cut_func=None, **cut_kwargs):
     df = _get_df(filepath)
-    if difference_cut:
-        dfs = _cut_df(df, difference_cut)
+    if cut_func:
+        dfs = cut_func(df, **cut_kwargs)
         data = [(df.sender, df.time, df.time.max()) for df in dfs]
         ds = tf.data.Dataset.from_generator(lambda: data, (tf.int32, tf.float32, tf.float32))
         return ds, len(dfs)
@@ -61,10 +61,10 @@ def to_event_dataset_sender_only(filepath, difference_cut=None):
     ds = tf.data.Dataset.from_tensors((df.sender, df.time, df.time.max()))
     return ds, 1
 
-def to_event_dataset_full(filepath, difference_cut=None):
+def to_event_dataset_full(filepath, cut_func=None, **cut_kwargs):
     df = _get_df(filepath)
-    if difference_cut:
-        dfs = _cut_df(df, difference_cut)
+    if cut_func:
+        dfs = cut_func(df, **cut_kwargs)
         data = [(df.sender, df.recipient, df.time, df.time.max()) for df in dfs]
         ds = tf.data.Dataset.from_generator(lambda: data, (tf.int32, tf.int32, tf.float32, tf.float32))
         return ds, len(dfs)

b6aedfc85a70a3f0e4ca11657935c0c45e4ffbc1
ArsArkanum95 <m.jozwiak@iambot.ai>
2019-04-02 21:14:59 +0200

    Experiments config update

--- experiments/config.py
+++ experiments/config.py
@@ -2,7 +2,11 @@ def sample_function(**kwargs):
     print(kwargs)
     return 5
 
-RUN_1 = {
+def simple_hook():
+    pass
+
+
+BASE_RUN = {
     'function': sample_function,
     'default_values': {
         'num_epochs': 10,
@@ -14,14 +18,63 @@ RUN_1 = {
         'data_mode': 'full'
     },
     'values': {
-        'data_path': ['kek', 'lol', 'eh'],
+        'data_path': [
+            '../data/edgelist_data/fb-forum/data.txt',
+            '../data/edgelist_data/ia-contacts_hypertext2009/data.txt'   
+        ],
+        'num_types': [899, 113],
         'num_units': [64, 256, 512],
         'vstate_len': [50, 200, 300]
     },
     'run': {
+        'individual_search': ['num_units', 'vstate_len']
+    },
+    'post_hook': simple_hook
+}
+
+
+RUN_1 = {
+    **BASE_RUN,
+    'default_values': {
+        **BASE_RUN['default_values'],
+        'batching_mode': 'gap_cut'
+    },
+    'values': {
+        **BASE_RUN['values'],
+        'batching_kwargs': [
+            {'min_gap_size': 10},
+            {'min_gap_size': 20}
+        ]
+    },
+    'run': {
         'individual_search': {
-            'args': ['data_path'],
+            'args': [('data_path', 'num_types', 'batching_kwargs')],
             'individual_search': ['num_units', 'vstate_len']
+        }
+    }
+}
+
+
+RUN_2 = {
+    **BASE_RUN,
+    'default_values': {
+        **BASE_RUN['default_values'],
+        'batching_mode': 'even_cut'
+    },
+    'values': {
+        **BASE_RUN['values'],
+        'batching_kwargs': [
+            {'piece_len': 10},
+            {'piece_len': 100}
+        ]
+    },
+    'run': {
+        'individual_search': {
+            'args': [('data_path', 'num_types')],
+            'individual_search': {
+                'args': ['batching_kwargs'],
+                'individual_search': ['num_units', 'vstate_len']
+            }
         }
     }
 }

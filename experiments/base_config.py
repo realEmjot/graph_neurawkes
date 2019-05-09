16437881c3e23fc4a9affb129471fb6eaa665484
Michał Jóźwiak <michal.jozwiak95@gmail.com>
2019-05-09 19:36:49 +0200

    Experiments v2 - full tests config

--- experiments/base_config.py
+++ experiments/base_config.py
@@ -9,24 +9,11 @@ from graph_neurawkes import train_and_save
 BASE_RUN = {
     'function': train_and_save,
     'default_values': {
-        'num_epochs': 10,
+        'num_epochs': 30,
         'batch_size': 1,
-        'num_units': 128,
-        'vstate_len': 100,
         'val_ratio': 0.2,
         'data_type': 'edgelist',
         'data_mode': 'full'
-    },
-    'values': {
-        'data_path': [
-            '../data/edgelist_data/fb-forum/data.csv',
-            '../data/edgelist_data/ia-contacts_hypertext2009/data.csv',
-            '../data/edgelist_data/ia-radoslaw-email/data.csv'
-        ],
-        'self_links': [True, False, True],
-        'num_types': [899, 113, 167],
-        'num_units': [64, 256, 512],
-        'vstate_len': [50, 200, 300]
     },
     'post_hook': tf.reset_default_graph
 }
--- experiments/config_full.py
+++ experiments/config_full.py
@@ -1,106 +1,29 @@
 from base_config import BASE_RUN
 
 
-RUN_full_gap_fb = {
+RUN_full = {
     **BASE_RUN,
-    'default_values': {
-        **BASE_RUN['default_values'],
-        'batching_mode': 'gap_cut',
-        'data_path': '../data/edgelist_data/fb-forum/data.csv',
-        'num_types': 899,
-        'model_savepath': 'full_saved_models/full_gap_fb/${id}',
-        'self_links': True
-    },
     'values': {
-        **BASE_RUN['values'],
+        'data_path': [
+            '../data/edgelist_data/fb-forum/data.csv',
+            '../data/edgelist_data/ia-contacts_hypertext2009/data.csv',
+            '../data/edgelist_data/ia-radoslaw-email/data.csv'
+        ],
+        'self_links': [True, False, True],
+        'num_types': [899, 113, 167],
         'batching_kwargs': [
-            {'min_gap_size': 1000},
-            {'min_gap_size': 2500},
-            {'min_gap_size': 5000}
-        ]
-    },
-    'run': {
-        'individual_search': {
-            'args': ['batching_kwargs'],
-            'individual_search': ['num_units', 'vstate_len']
-        }
-    }
-}
-
-
-RUN_full_gap_hypertext = {
-    **BASE_RUN,
-    'default_values': {
-        **BASE_RUN['default_values'],
-        'batching_mode': 'gap_cut',
-        'data_path': '../data/edgelist_data/ia-contacts_hypertext2009/data.csv',
-        'num_types': 113,
-        'model_savepath': 'full_saved_models/full_gap_hypertext/${id}',
-        'self_links': False
-    },
-    'values': {
-        **BASE_RUN['values'],
-        'batching_kwargs': [
-            {'min_gap_size': 20},
-            {'min_gap_size': 50},
-            {'min_gap_size': 75}
-        ]
-    },
-    'run': {
-        'individual_search': {
-            'args': ['batching_kwargs'],
-            'individual_search': ['num_units', 'vstate_len']
-        }
-    }
-}
-
-RUN_full_gap_radoslaw = {
-    **BASE_RUN,
-    'default_values': {
-        **BASE_RUN['default_values'],
-        'batching_mode': 'gap_cut',
-        'data_path': '../data/edgelist_data/ia-radoslaw-email/data.csv',
-        'num_types': 167,
-        'model_savepath': 'full_saved_models/full_gap_radoslaw/${id}',
-        'self_links': True
-    },
-    'values': {
-        **BASE_RUN['values'],
-        'batching_kwargs': [
-            {'min_gap_size': 15000},
-            {'min_gap_size': 30000},
+            {'min_gap_size': 5000},
+            {'min_gap_size': 75},
             {'min_gap_size': 45000}
-        ]
-    },
-    'run': {
-        'individual_search': {
-            'args': ['batching_kwargs'],
-            'individual_search': ['num_units', 'vstate_len']
-        }
-    }
-}
+        ],
 
-RUN_full_even = {
-    **BASE_RUN,
-    'default_values': {
-        **BASE_RUN['default_values'],
-        'batching_mode': 'even_cut'
-    },
-    'values': {
-        **BASE_RUN['values'],
-        'batching_kwargs': [
-            {'piece_len': 100},
-            {'piece_len': 250},
-            {'piece_len': 500}
-        ]
+        'num_units': [64, 128, 256, 512],
+        'vstate_len': [50, 100, 200, 300]
     },
     'run': {
         'individual_search': {
-            'args': [('data_path', 'self_links', 'num_types')],
-            'individual_search': {
-                'args': ['batching_kwargs'],
-                'individual_search': ['num_units', 'vstate_len']
-            }
+            'args': [('data_path', 'self_links', 'num_types', 'batching_kwargs')],
+            'grid_search': ['num_units', 'vstate_len']
         }
     }
 }
